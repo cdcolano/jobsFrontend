ed4d3b6998f097395949a183e3032e8ab5c9d2ec
cdcolano <71667511+cdcolano@users.noreply.github.com>
2024-03-07 18:19:09 +0000

    Add files via upload

--- services/frontend/src/stores/searchStore.js
+++ services/frontend/src/stores/searchStore.js
@@ -15,10 +15,32 @@ export const useSearchStore = defineStore(
 
         watch(query, async () => {
             results.value = null
-            fetch('/api/search/?query=' + query.value)
+            fetch('http://localhost:5001/search/?query=' + query.value)
                 .then(response => response.json())
                 .then(data => results.value = data);
         })
 
         return {query, get_query, get_results, search}
     })
+
+
+export const useSuggestStore = defineStore( 'suggest' ,
+        ()=> {
+            const query = ref('');
+            const results = ref(null);
+            const get_query = computed(() => query.value);
+            const get_results = computed(() => results.value)    
+            let  suggest = (q)=> { 
+                query.value = q
+            }
+
+            watch(query, async () => {
+                results.value = null
+                fetch('http://localhost:5001/suggest/?query=' + query.value)
+                    .then(response => response.json())
+                    .then(data => results.value = data);
+            })
+            // Replace `yourServerApiEndpoint` with the actual endpoint
+            return {query, get_query, get_results, suggest}
+        }
+)
